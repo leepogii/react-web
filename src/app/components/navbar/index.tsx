5cff0d8d2d9020d70ccc3553895e04488e47b719
Min H Lee <leepogii@gmail.com>
2021-08-11 21:44:06 -0700

    Start coding

---
+++ src/app/components/navbar/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const NavbarContainer = styled.div`
+    min-height: 68px;
+    ${tw`
+        w-full
+        max-w-2xl
+        flex
+        flex-row
+        items-center
+        lg:
+    `};
+`;
